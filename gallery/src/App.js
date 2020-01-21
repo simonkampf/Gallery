fa560ba03413ecf154e0f30191f2918d55f7bb40
Simon <simonhehehe@gmail.com>
2020-01-21 12:44:37 +0100

    catch request-error

--- gallery/src/App.js
+++ gallery/src/App.js
@@ -10,6 +10,7 @@ const App = () => {
   const [loading, setLoading] = useState(false); // if we're waiting for the rest-response
   const [pageNumber, setPageNumber] = useState(1); // page number for next request
   const [allowMoreProfiles, setAllowMoreProfiles] = useState(true); // max profiles = 200
+  const [error, setError] = useState(false);
   useScroll(loading); // attach scroll-location-restore to loading boolean...
 
   const fetchProfiles = async () => {
@@ -19,11 +20,15 @@ const App = () => {
     setLoading(true);
     var url = "https://randomuser.me/api/?results=40&page=" + pageNumber;
     console.log(url);
-    setPageNumber(pageNumber + 1);
-    const res = await axios.get(url);
-    console.log(res.data);
-
-    setProfiles(profiles => [...profiles, ...res.data.results]); // append 40 new profiles to previous old data (or append to empty array if first)
+    try {
+      const res = await axios.get(url);
+      setProfiles(profiles => [...profiles, ...res.data.results]); // append 40 new profiles to previous old data (or append to empty array if first)s
+      console.log(res.data);
+      setPageNumber(pageNumber + 1);
+    } catch (e) {
+      setError(true);
+      console.log("Error sending GET-request");
+    }
 
     setLoading(false);
     if (pageNumber > 5) {
@@ -47,11 +52,17 @@ const App = () => {
       <h1>Secret Profiles</h1>
 
       <Profiles profiles={profiles} loading={loading}></Profiles>
-      {loading === false && allowMoreProfiles === true ? ( // dont display button whilst loading or if we have reached limit for number of profiles (200)
-        <button onClick={loadMore} className="btn btn-info">
-          Load more
-        </button>
-      ) : null}
+      {error == false ? (
+        loading === false && allowMoreProfiles === true ? ( // dont display button whilst loading or if we have reached limit for number of profiles (200)
+          <button onClick={loadMore} className="btn btn-info">
+            Load more
+          </button>
+        ) : null
+      ) : (
+        <div class="alert alert-danger" role="alert">
+          Something went wrong...
+        </div>
+      )}
     </div>
   );
 };
